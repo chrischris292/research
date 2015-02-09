e4a0a3f93c46205481436b377a262a4df7b23f2c
chrischris292 <cbchan2@illinois.edu>
2015-02-09 00:17:46 -0600

    Implemented MSE and tests
    separated training and test queries
    implemented mean squared error
    gives .009 error…interesting to try and test on correct data set in
    future

--- knn.py
+++ knn.py
@@ -10,19 +10,49 @@ from sklearn.neighbors import KNeighborsClassifier
 import numpy as np
 X = []
 y = []
-with open("outcome.csv") as csvfile:
-    levelOne = csv.reader(csvfile, delimiter=',')
-    for row in levelOne:
+lengthOutcome = 11831
+trainingSize = (lengthOutcome/4)*3
+
+def classify(Xi,yi,testData):
+    print("finshed importing data")
+    from sklearn.neighbors import KNeighborsClassifier
+    neigh = KNeighborsClassifier(n_neighbors=30)
+    neigh.fit(Xi, yi)
+    squaredErrorSum = 0.0;
+    for test in testData:
+        'get classification'
+        classification = neigh.predict(test[1:len(test)-1])
+        classification = float(classification[0])
+        'compare to actual'
+        'sum squared errors'
+        actual = float(test[len(test)-1])
+        squaredError = pow((classification - actual),2)
+        squaredErrorSum = squaredErrorSum + squaredError
+
+    meanSquaredError = squaredErrorSum/lengthOutcome;
+    print(meanSquaredError)
+
+def main():
+    testData = []
+    with open("outcome.csv") as csvfile:
+        levelOne = csv.reader(csvfile, delimiter=',')
         count = 0
-        endIndex = len(row)-1
-        row = map(float, row)
-        X.append(row[1:endIndex])
-        y.append(row[endIndex])
+        for row in levelOne:
+            'Load Training Data'
+            if count<=trainingSize:
+                X.append(row[1:len(row)-1])
+                y.append(row[len(row)-1])
+                count = count+1
+            elif(trainingSize<count and count<lengthOutcome):
+                row = map(float, row)
+                testData.append(row)
+                count = count+1
+        classify(X,y,testData)
+
+main()
 
 
-print("finshed importing data")
-from sklearn.neighbors import KNeighborsClassifier
-neigh = KNeighborsClassifier(n_neighbors=30)
-neigh.fit(X, y)
-print(neigh.predict([33809.0,3382.295,0.23306856,301.99652,5.52761,-6.9788985,-4.086555]))
+
+
+
 
